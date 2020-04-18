ab457737013740631a33da63de6d3e80d8a47753
eunery <asdad@yaofj.com>
2020-04-18 16:24:37 +0500

    Addition

--- GameLabNumber2/GameLabNumber2/Level5.xaml.cs
+++ GameLabNumber2/GameLabNumber2/Level5.xaml.cs
@@ -14,14 +14,44 @@ using System.Windows.Shapes;
 
 namespace GameLabNumber2
 {
-    /// <summary>
-    /// Логика взаимодействия для Level5.xaml
-    /// </summary>
     public partial class Level5 : Window
     {
+        private int count = 0;
         public Level5()
         {
             InitializeComponent();
+            answerButton.Click += answerButton_Click;
+        }
+        private void answerButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (tbAnswer.Text == "") tbAnswer.Clear();
+            string[] str = tbAnswer.Text.Split(' ');
+            string str0 = str[0];
+            string str1 = str[1];
+            switch (str0)
+            {
+                case "1":
+                    if (str[1] == "4")
+                    {
+                        count++;
+                        logLabel.Content = logLabel.Content + "1 = Правильно";
+                    }
+                    break;
+                case "2":
+                    if (str[1] == "7")
+                    {
+                        count++;
+                        logLabel.Content = logLabel.Content + "2 = Правильно";
+                    }
+                        break;
+            }
+            if (count == 2)
+            {
+                Level6 lvl6 = new Level6();
+                lvl6.Show();
+                LevelFive.Close();
+            }
+            tbAnswer.Clear();
         }
     }
 }
